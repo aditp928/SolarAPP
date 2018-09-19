c5ee940eb4378ec4a970df4dd2466139ae133dd5
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-09-18 20:34:11 -0400

    working on user auth

--- solar/api/v1/auth/passport/index.js
+++ solar/api/v1/auth/passport/index.js
@@ -3,7 +3,6 @@ const LocalStrategy = require('./localStrategy')
 const db = require('../../models')
 
 /**
-* Saves user_id to req.session.passport.user
 * @function
 * @param {Object} req : req.session.passport.user = {id: user_id}
 * @param {Object} res 
@@ -14,7 +13,6 @@ passport.serializeUser((user, done) => {
 	done(null, { id: user._id })
 })
 
-// user object attaches to the request as req.user
 passport.deserializeUser((id, done) => {
 	console.log('DeserializeUser called')
 	db.User.findOne(
@@ -28,7 +26,7 @@ passport.deserializeUser((id, done) => {
 	)
 })
 
-//  Use Strategies 
+//  Use Strategies
 passport.use(LocalStrategy)
 
 module.exports = passport
